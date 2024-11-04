f013a68a01b58924ae5ac6bb894f27042ea640f4
kiryazovkosta <kosta.kiryazov@gmail.com>
2024-11-05 01:27:10 +0200

    Update split

--- src/02_Fundamentals/03_Lists_Basics/seize_the_fire.py
+++ src/02_Fundamentals/03_Lists_Basics/seize_the_fire.py
@@ -4,9 +4,7 @@ effort = 0
 used_water = 0
 print("Cells:")
 for file_level in fire_levels:
-    fire_data = file_level.split(" = ")
-    level = fire_data[0]
-    value = int(fire_data[1])
+    level, value = file_level.split(" = ", 1); value = int(value)
     if ((level == "High" and (81 <= value <= 125))
             or (level == "Medium" and (51 <= value <= 80))
             or (level == "Low" and (1 <= value <= 50))):
