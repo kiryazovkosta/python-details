dba1936f68f01fb825f6e4367878f8fbb912aa4f
kiryazovkosta <kosta.kiryazov@gmail.com>
2025-12-07 16:33:18 +0200

    Add Advent of code 2025 Day 7

--- src/Advent_of_Code/2025/Day07/laboratories.py
+++ src/Advent_of_Code/2025/Day07/laboratories.py
@@ -20,11 +20,32 @@ def first(filename: str) -> int:
     return tachyon_beams
 
 def second(filename: str) -> int:
-    return 0
+    matrix = [list(line) for line in read_input(filename)]
+    paths = {matrix[0].index("S"): 1}
+    
+    for rowIndex in range(1, len(matrix)):
+        indices = set([i for i, x in enumerate(matrix[rowIndex]) if x == '^'])
+        
+        if len(indices) == 0:
+            continue
+
+        new_paths = {}
+        tz = set(paths.keys()).intersection(indices)
+        for z in tz:
+            new_paths[z - 1] = new_paths.get(z - 1, 0) + paths[z]
+            new_paths[z + 1] = new_paths.get(z + 1, 0) + paths[z]
+        
+        for col in paths:
+            if col not in tz:
+                new_paths[col] = new_paths.get(col, 0) + paths[col]
+        
+        paths = new_paths
+
+    return sum(paths.values())
 
 def main():
-    print(f"First task: {first('Day07/test.txt')}")
-    print(f"Second task: {second('Day07/test.txt')}")
+    print(f"First task: {first('Day07/input.txt')}")
+    print(f"Second task: {second('Day07/input.txt')}")
 
 if __name__ == "__main__":
     main()
